798a96f2d9a208d7a8782226ef34008deeda8126
Jacob Fisher <99689919+OpaqueJake@users.noreply.github.com>
2023-04-08 10:02:57 -0700

    Added Comments to show what each method does

--- VoiceMailSystem/src/mailBox.java
+++ VoiceMailSystem/src/mailBox.java
@@ -4,11 +4,13 @@ import java.util.*;
 
 public class mailBox {
 	
+	// Creates linked lists to allow for the creation of new messages to be received and the saving of those messages
 	static LinkedList<String> newMessageQueuec1 = new LinkedList<String>();
 	static LinkedList<String> savedMessageQueuec1 = new LinkedList<String>();
 	static LinkedList<String> newMessageQueuec2 = new LinkedList<String>();
 	static LinkedList<String> savedMessageQueuec2 = new LinkedList<String>();
 
+	// Adds message created by caller B to caller A's message box 
 	public static void addMessagec1(String message) {
 		if(!message.equalsIgnoreCase("h")) {
 			newMessageQueuec1.add(message);
@@ -28,6 +30,7 @@ public class mailBox {
 	public static void deleteSavedMessagec1(int c1) {
 		savedMessageQueuec1.remove(c1);
 	}
+	// Adds message created by caller A to caller B's message box
 	public static void addMessagec2(String message) {
 		if(!message.equalsIgnoreCase("h")) {
 			newMessageQueuec2.add(message);
